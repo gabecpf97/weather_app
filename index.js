fd9287c83665440ea8d2f6460ad12a86a5fdb2e3
Gabriel <gabrielcpf2997@gmail.com>
2021-11-16 19:01:09 -0800

    finished css and should work fine now

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ async function _getWeatherWLoc(location, units) {
     try {
         const response = await fetch(url, {mode: 'cors'});
         const data = await response.json();
-        console.log(data);
-        _displayResult(data);
+        _displayResult(data, location, units);
     } catch (e) {
         console.log(e);
     }
@@ -36,24 +35,28 @@ function _getInput() {
     }
 }
 
-function _displayResult(data) {
+function _displayResult(data, location, units) {
     const display = document.querySelector('.display');
-    const data_div = _createDivForData(data);
+    const data_div = _createDivForData(data, units);
+    const cityName = location.replace(/^\w/, (c) => c.toUpperCase());
+    const fields_div = _createEle('div', 'holder', '');
+    display.appendChild(_createEle('h2', 'city', cityName + "'s weather"));
     for (let field in data_div) {
-        display.append(data_div[field]);
+        fields_div.appendChild(data_div[field]);
     }
+    display.appendChild(fields_div);
 }
 
-const _createDivForData = (data) => {
-    const data_w_main = _createEle('p', 'w_main', data.weather[0].main);
-    const data_w_des = _createEle('p', 'w_des', data.weather[0].description);
-    const data_main_t = _createEle('p', 'm_t', data.main.temp);
-    const data_main_fl = _createEle('p', 'm_fl', data.main.feels_like);
-    const data_main_p = _createEle('p', 'm_p', data.main.pressure);
-    const data_main_h = _createEle('p', 'm_h', data.main.humidity);
-    const data_main_min = _createEle('p', 'm_t_min', data.main.temp_min);
-    const data_main_max = _createEle('p', 'm_t_max', data.main.temp_max);
-    const data_wind = _createEle('p', 'm_w', data.wind.speed);
+const _createDivForData = (data, units) => {
+    const data_w_main = _createfields(units, 'p', 'weather', data.weather[0].main);
+    const data_w_des = _createfields(units, 'p', 'description', data.weather[0].description);
+    const data_main_t = _createfields(units, 'p', 'temp', data.main.temp);
+    const data_main_fl = _createfields(units, 'p', 'feels_like', data.main.feels_like);
+    const data_main_p = _createfields(units, 'p', 'pressure', data.main.pressure);
+    const data_main_h = _createfields(units, 'p', 'humidity', data.main.humidity);
+    const data_main_min = _createfields(units, 'p', 'min_temp', data.main.temp_min);
+    const data_main_max = _createfields(units, 'p', 'max_temp', data.main.temp_max);
+    const data_wind = _createfields(units, 'p', 'wind_speed', data.wind.speed);
 
     return {data_w_main, data_w_des, data_main_t, data_main_fl, data_main_p, data_main_h,
                 data_main_min, data_main_max, data_wind};
@@ -64,10 +67,68 @@ function _clearDisplay(display) {
         display.removeChild(display.firstChild);
 }
 
+function _createfields(units, type, name, value) {
+    const data_div = _createEle('div', 'fields', '');
+    const field_name = name.replace('_', ' ');
+    const labelText = `${field_name}`;
+    const output = value + _getUnit(units, name);
+    const label = _createEle('label', `label_${name}`, labelText);
+    const element = _createEle(type, name, output);
+    data_div.appendChild(label);
+    data_div.appendChild(element);
+    return data_div;
+}
+
 function _createEle(type, name, value) {
     const element = document.createElement(type);
     element.classList.add(name);
     element.textContent = value;
-    console.log(value);
     return element;
+}
+
+function _getUnit(unit, name) {
+    let value = " ";
+    switch (name) {
+        case 'temp':
+        case 'feels_like':
+        case 'min_temp:':
+        case 'max_temp':
+            switch (unit) {
+                case 'metric':
+                    value += '°C';
+                    break;
+
+                case 'imperial':
+                    value += '°F';
+                    break;
+
+                case 'standard':
+                    value += 'K';
+                    break;
+            }   
+            break;
+
+        case 'pressure':
+            value += 'hPa';
+            break;
+
+        case 'humidity':
+            value += '%';
+            break;
+
+        case 'wind_speed':
+            switch (unit) {
+                case 'metric':
+                case 'standard':
+                    value += 'm/s';
+                    break;
+
+                case 'imperial':
+                    value += 'miles/hour';
+                    break;
+            }
+            break;
+    }
+
+    return value;
 }
